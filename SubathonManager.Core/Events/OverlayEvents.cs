b4bdfb0886d22103018d5715769e49d11b608836
WolfwithSword <wolfwithsword4426@gmail.com>
2025-10-26 16:36:40 -0300

    reload overlay pages on update if matches (#47)

---
+++ SubathonManager.Core/Events/OverlayEvents.cs
@@ -0,0 +1,10 @@
+﻿namespace SubathonManager.Core.Events;
+
+public static class OverlayEvents
+{
+    public static event Action<Guid>? OverlayRefreshRequested;
+    public static void RaiseOverlayRefreshRequested(Guid routeGuid)
+    {
+        OverlayRefreshRequested?.Invoke(routeGuid);
+    }
+}
