0e6b9cb41e1aea5cf48461ecca1f59476539aa2e
WolfwithSword <12175651+WolfwithSword@users.noreply.github.com>
2025-11-01 18:04:19 -0300

    add visibility toggle to widgets, closes #45

---
+++ SubathonManager.Data/Migrations/20251101204156_addWidgetVisibility.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SubathonManager.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addWidgetVisibility : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Visibility",
+                table: "Widgets",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Visibility",
+                table: "Widgets");
+        }
+    }
+}
