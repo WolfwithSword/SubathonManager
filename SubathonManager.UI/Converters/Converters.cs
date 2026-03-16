4aa2101c0edb1596748e888ff076d9f3fa33a0b3
WolfwithSword <12175651+WolfwithSword@users.noreply.github.com>
2026-03-16 13:11:37 -0300

    update jsvar editor code to UI controlled

--- SubathonManager.UI/Converters/Converters.cs
+++ SubathonManager.UI/Converters/Converters.cs
@@ -228,4 +228,12 @@ namespace SubathonManager.UI.Converters
         public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
             => throw new NotImplementedException();
     }
+    
+    public class StringToBoolConverter : IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+            => bool.TryParse(value as string, out var b) && b;
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+            => (value is bool ? value.ToString() : "False") ?? "False";
+    }
 }
